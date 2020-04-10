35ec78ef4abb32702deab5e96ecddad8cc6da4f3
Luis Antonio <luis.antonio.v.r.90@gmail.com>
2020-04-10 15:40:09 -0500

    shuffle

--- thehiden.py
+++ thehiden.py
@@ -1,20 +1,44 @@
+"""
+***************************************************
+This script accomplish an automated task that consist
+in open a note pad and start copying the text stored
+in files with the extensions written in the glob
+function.
+
+At the end, the text will be saved
+and finally the notepad it will be closed.
+
+****************************************************
+"""
 import os
 import pyautogui
+from glob import glob
 from time import sleep
+from random import shuffle
 
-
+# open notepad
 os.popen('notepad')
 sleep(0.5)
 
-file = open('file_test.txt', mode='r')
-pyautogui.write(file.read(), interval=0.15)
-file.close()
+# read all files with the specified extension
+files = glob('*.txt')
 
+# random
+shuffle(files)
+
+# coping text
+for path in files:
+
+    file = open(path, mode='r')
+    pyautogui.write(file.read(), interval=0.15)
+    file.close()
+
+# save and close notepad
 pyautogui.hotkey('ctrl', 'g')
-sleep(0.5)
+sleep(2)
 
-pyautogui.write('outfile.txt', interval=0.05)
-sleep(0.5)
+pyautogui.write('outfile.txt', interval=0.5)
+sleep(1)
 
 pyautogui.press('enter')
 sleep(0.5)
